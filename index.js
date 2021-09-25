d96eefd77680b94e71da145fcad48860e09e1549
rjdaw <daw19003@byui.edu>
2021-09-24 22:35:13 -0600

    Change port number

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
 
