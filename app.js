2db0ed4538cdb57edea065ffa88c0e5bb07b2d9f
rjdaw <daw19003@byui.edu>
2021-10-02 18:32:28 -0600

    prove03 mvc implementation

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/prove02/admin');
-const shopRoutes = require('./routes/prove02/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {
-        pageTitle: 'Page Not Found'
-    });
-});
+app.use(errorController.get404);
 
-app.listen(PORT);
+app.listen(3000);
